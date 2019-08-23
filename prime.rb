c2b4137d2bad20008a710bafe5462f21817dc0fe
Brad Alexander <brad.alexander913@gmail.com>
2019-08-23 01:06:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(n)
+  if (n <= 3)
+    return (n > 1)
+  elsif ((n % 2 == 0) || (n % 3 == 0)) 
+    return false 
+  else
+    i = 5
+    while ((i * i) <= n )
+      if ((n % i == 0) || (n % (i + 2) == 0)) 
+        return false
+        i += 1
+      end 
+    end 
+  return true
+  end
+end
+      
